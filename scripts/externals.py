bd24c001fce171cb44d003898f3bf24bb61e78b2
jmcarcell <jmcarcell@users.noreply.github.com>
2023-02-15 13:00:52 +0100

    Use an environment

--- scripts/externals.py
+++ scripts/externals.py
@@ -5,7 +5,7 @@ import re
 import yaml
 
 CONFIG = '/Package/spack/var/spack/environments/dev/spack.yaml'
-PACKAGES = '/root/.spack/packages.yaml'
+# PACKAGES = '/root/.spack/packages.yaml'
 EXTERNALS = ['root', 'vdt', 'cmake', 'python', 'py-cython', 'py-pip', 'libyaml', 'py-setuptools', 'py-wheel', 'py-markupsafe',
              'py-jinja2', 'py-pyyaml', 'py-six', 'nlohmann-json', 'intel-tbb', 'boost', 'gaudi', 'dd4hep', 'gdb', 'marlin',
              'catch2']
@@ -13,17 +13,17 @@ VERSION = re.compile('(:?\d+\.)+\d*')
 
 out = subprocess.check_output(f'spack external find {" ".join(EXTERNALS)}'.split()).decode()
 print(out)
-with open(PACKAGES) as f:
+with open(CONFIG) as f:
     base = yaml.safe_load(f.read())
 for p in EXTERNALS:
-    if f'{p}@' not in out and p not in base['packages']:
+    if f'{p}@' not in out and p not in base['spack']['packages']:
         path = [x for x in os.environ['CMAKE_PREFIX_PATH'].split(':') if f'/{p}/' in x]
         if not path:
             print(f'Unable to find external {p}')
             continue
         path = path[0]
         version = re.search(VERSION, path).group(0)
-        base['packages'][p] = {'externals': [{'spec': f'{p}@{version}', 'prefix': f'{path}'}]}
+        base['spack']['packages'][p] = {'externals': [{'spec': f'{p}@{version}', 'prefix': f'{path}'}]}
 yaml.dump(base, open(PACKAGES, 'w'))
 
 # When A depends on B which depends on C but B is an external,
