658f8fa40377d6914b546fe8bde47e5494a3c205
jmcarcell <jmcarcell@users.noreply.github.com>
2023-07-25 21:51:44 +0200

    Update the script for syncing

---
+++ scripts/sync-files.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# This script is used to sync files from to the destination in
+# all the repositories in /key4hep-dev-utils/scripts/get_packages.sh
+
+# Usage example:
+# ./sync-ci-workflow.sh /path/to/destination/ /absolute/path/to/file1 /absolute/path/to/file2
+
+
+TOP_DIR=$(git rev-parse --show-toplevel)
+source $TOP_DIR/scripts/get_packages.sh
+
+
+function git_checkout_and_update {
+  packages_list_name=$1[@]
+  destination=$2
+  files_to_sync=("${@:3}")
+  packages_list=("${!packages_list_name}")
+  for package_name in "${packages_list[@]}"; do
+    echo "Syncing package $package_name"
+    git clone --quiet git@github.com:key4hep/${package_name}.git --depth 1
+    pushd ${package_name}
+    for file in "${files_to_sync[@]}"; do
+      cp $file $destination
+      git add $destination/$file
+    done
+    old_message=`git log -1|grep -v "^commit"`
+    git commit -am "Syncing $(basename $files_to_sync); previous commit: ${old_message}"
+    git push --quiet
+    popd
+  done
+}
+
+tmp_dir=$(mktemp -d -t tmp_XXXXXXXXXX)
+
+pushd $tmp_dir
+
+files=("${@:2}")
+dest=$1
+
+echo "Will sync the following files to $dest"
+for file in "${files[@]}"; do
+  echo $file
+done
+
+git_checkout_and_update packages $dest "${files[@]}"
+
+popd
+
+rm -rf $tmp_dir
