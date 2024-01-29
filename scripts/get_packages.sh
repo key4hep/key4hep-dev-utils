93d4a24dbed0152f2e6b1ca7661b439ea155c48b
jmcarcell <jmcarcell@users.noreply.github.com>
2024-01-29 08:36:55 +0100

    Remove k4Lcioreader

--- scripts/get_packages.sh
+++ scripts/get_packages.sh
@@ -4,9 +4,11 @@ packages=(
     "k4edm4hep2lcioconv"
     "k4fwcore"
     "k4geo"
-    "k4lcioreader"
     "k4marlinwrapper"
     "k4simdelphes"
     "k4actstracking"
     "k4-project-template"
+    "k4reco"
+    # "CLDConfig"
+    # "key4hep-tutorials"
     )
