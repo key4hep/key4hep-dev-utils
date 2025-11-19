865b97cd2ac5f5c89bc3c28a3b4954a84855ccfd
Juan Miguel Carceller <jmcarcell@users.noreply.github.com>
2025-11-19 08:51:05 +0100

    Add a script to update the pre-commit config of a repository

---
+++ scripts/update-pre-commit-config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script is used to update the pre-commit configuration for a single repository
+
+# Usage example:
+# ./update-pre-commit-settings.sh organization/repo
+
+set -e
+
+org=$(dirname "$1")
+repo=$(basename "$1")
+
+git clone https://github.com/$org/$repo.git
+python merge_pre_commit.py --global-config .pre-commit-config.yaml --local-config ${repo}/.github/pre-commit-config-local.yaml
+mv generated.yaml ${repo}/.pre-commit-config.yaml
+cd ${repo}
+git add .pre-commit-config.yaml
+git commit -m "Update pre-commit configuration"
+# git push --quiet
+
+echo "Updated the pre-commit configuration for $repo"
